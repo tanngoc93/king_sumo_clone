04021062e0e485a92cfd2008c9b4681d9b16e011
ngocnt <tanngoc93@gmail.com>
2022-05-30 10:49:02 +0700

    Update app/sidekiq/destroy_orphan_image_job.rb

--- app/sidekiq/destroy_orphan_image_job.rb
+++ app/sidekiq/destroy_orphan_image_job.rb
@@ -1,6 +1,8 @@
 class DestroyOrphanImageJob
   include Sidekiq::Job
 
+  sidekiq_options retry: false
+
   def perform(id, *args)
     image = Image.find_by(id: id)
     return if image && image.campaign_id.present?
