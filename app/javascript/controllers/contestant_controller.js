731a959d025ce33411396fd678cb91d37186644d
ngocnt <tanngoc93@gmail.com>
2022-06-02 15:26:33 +0700

    Delete app/javascript/controllers/campaign_controller.js

--- app/javascript/controllers/contestant_controller.js
+++ app/javascript/controllers/contestant_controller.js
@@ -77,4 +77,30 @@ export default class extends Controller {
     })
   }
 
+  ajaxResendConfirmation(event) {
+    event.preventDefault()
+
+    const { secretCode } = event.target.dataset
+
+    $.ajax({
+      method: "POST",
+      url: this.data.get("url"),
+      contentType: "application/x-www-form-urlencoded; charset=utf-8",
+      cache: false,
+      dataType: "JSON",
+      beforeSend: function() {
+        // do something
+      }
+    })
+    .done(function(data) {
+      console.log(">>>", data["message"])
+    })
+    .fail(function() {
+      alert("Something went wrong...")
+    })
+    .always(function () {
+      // always do something
+    })
+  }
+
 }
